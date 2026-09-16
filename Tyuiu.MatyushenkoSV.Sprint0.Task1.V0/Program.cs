e1b57d74a8da2d1fd0b1d388d82b3132853c25ce
Савелий Матюшенко <Tyuiu.MatyushenkoSV@outlook.com>
2026-09-16 20:16:15 +0500

    Task1

---
+++ Tyuiu.MatyushenkoSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.MatyushenkoSV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Савва";
+            int age = 18;
+            Console.WriteLine("Здравстуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
