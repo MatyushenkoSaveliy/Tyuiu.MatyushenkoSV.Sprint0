bcebbc077b52f7dbc68c7ea3825f2e9d7bb061f1
Савелий Матюшенко <Tyuiu.MatyushenkoSV@outlook.com>
2026-09-16 21:59:42 +0500

    Task2

---
+++ Tyuiu.MatyushenkoSV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MatyushenkoSV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MatyushenkoSV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Савва";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Савва", res);
+        }
+    }
+}
